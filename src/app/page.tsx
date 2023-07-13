72b50917964f56dc7e37bdf0852c8f4fb8734c95
Diego Goulart <diego.goulart@edifyeducation.com.br>
2023-07-13 20:26:35 -0300

    feat: :sparkles: wip - create heroform

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
-import { Room } from '@/components/Room'
-import gridStyle from './page.module.css'
+// import { Room } from '@/components/Room'
+// import gridStyle from './page.module.css'
+import HeroForm from '@/components/HeroForm'
+// import HeroSection from '@/components/HeroSection'
 
 export default function Home() {
   return (
-    <section className="m-auto flex w-full max-w-screen-xl flex-row justify-center">
-      <div
+    <section className="m-auto flex w-full flex-row justify-center px-4">
+      {/* <div
         className={`relative mt-8 h-auto w-full gap-4 ${gridStyle.grid_template}`}
       >
         <Room name="Squad Percy" isOpen haveMembers haveOnlineMembers />
         <Room name="Squad Percy" haveMembers />
         <Room name="Squad Percy" />
-      </div>
+      </div> */}
+      {/* <HeroSection /> */}
+      <HeroForm />
     </section>
   )
 }
