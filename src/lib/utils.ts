575e9df57e4e4c777739921bc5430e74225e933f
Rafael Cruz Rosa <rafaelcruzrosa1@gmail.com>
2023-09-23 13:54:50 -0300

    Function to decode base64

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+function base64ToBytes(base64: string) {
+  const binString = atob(base64);
+  return Uint8Array.from(binString, (m) => m.codePointAt(0));
+}
+
+export function decodeBase64(base64: string) {
+  return new TextDecoder().decode(base64ToBytes(base64))
 }
